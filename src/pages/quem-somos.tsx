d17963ee625f3a571e97cb2743db08cf0eabe513
Joao <joaovittormtm@gmail.com>
2025-12-04 00:35:10 -0300

    Section 'Nossa história' criada

--- src/pages/quem-somos.tsx
+++ src/pages/quem-somos.tsx
@@ -16,8 +16,70 @@ function QuemSomos() {
                     <img src={fotoTime} alt="Foto do time" />
                 </div>
             </section>
+
+            <section className='px-6 py-10'>
+                <h1 className="large-title">Nossa história</h1>
+                <h3 className="font-medium">Alguns marcos da Estat Jr</h3>
+
+                <ul id="timeline" className="list-disc-none flex flex-col gap-2 mt-5 pt-5 border-l-4 border-black">
+                    <li>
+                        <strong>1991</strong>
+                        <p>Fundação da Estat Junior na UNICAMP. Primeiros projetos de consultoria estatística com a comunidade.</p>
+                    </li>
+                    <li>
+                        <strong>2000</strong>
+                        <p>Ampliação do portifólio: pesquisas de mercado, opinião e apoio a laboratórios e TCCs.</p>
+                    </li>
+                    <li>
+                        <strong>2010</strong>
+                        <p>Profissionalização de processos, certificações internas e aumento de impacto no MEJ.</p>
+                    </li>
+                    <li>
+                        <strong>2021</strong>
+                        <p>30 anos de Estat Junior: celebração da jornada e plano de futuro orientado a dados.</p>
+                    </li>
+                    <li>
+                        <strong>2025</strong>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus animi praesentium delectus?</p>
+                    </li>
+                </ul>
+            </section>
+
+            <TagStyles/>
         </main>
     );
 }
 
 export default QuemSomos
+
+export function TagStyles() {
+    return (
+        <style>{`
+            li::before {
+                display: inline-block;
+                margin-right: 5px;
+                content: "";
+                height: 12px;
+                width: 12px;
+                border-radius: 50%;
+                background-color: #5a94ff;
+            }
+
+            li {
+                margin-left:-8px;
+                min-height: 80px;
+            }
+
+            li p {
+                margin-left: 20px;
+                line-height: 1;
+                font-size: 14px;
+                font-weight: 500;
+            }
+            
+            li strong {
+                font-size: 24px;
+            }
+        `}</style>
+    );
+}
