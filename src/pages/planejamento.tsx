c43d4d2d60d8c6f91b8acacbb80dde3815be695a
Joao <joaovittormtm@gmail.com>
2025-12-16 23:43:37 -0300

    Planejamento: Seção 'Nossos pilares' feita

--- src/pages/planejamento.tsx
+++ src/pages/planejamento.tsx
@@ -19,6 +19,33 @@ function Planejamento() {
                     <p>Antes de iniciar a coleta, nós desenhamos a estrutura completa do seu projeto para garantir a validade estatística dos resultados.</p>
                 </div>
             </section>
+
+            <section className="pb-30">
+                <div className="bg-[#1A485B] text-white text-center px-5 pt-10 pb-25">
+                    <h1 className="font-bold text-3xl max-w-[600px] mx-auto">Nossos Pilares para um Estudo de Qualidade</h1>
+                </div>
+
+                <div className="sm:bg-linear-to-b from-[#1A485B] from-20% to-white to-20% text-center text-sm font-medium flex flex-wrap gap-x-10 justify-center gap-y-15">
+                    <div className="bg-white shadow shadow-neutral-900 aspect-10/11 max-w-[220px] sm:max-w-[280px] px-2 sm:px-8">
+                        <div className="bg-[#266C8B] min-h-10 text-white -translate-y-[50%] tracking-tighter py-2 px-2 box-content">
+                            <h3>Questionários e Formulários Otimizados</h3>
+                        </div>
+                        <p className="sm:text-lg">Criamos ferramentas de coleta de dados claras e focadas para extrair as respostas que realmente importam para os seus objetivos.</p>
+                    </div>
+                    <div className="bg-white shadow shadow-neutral-900 aspect-10/11 max-w-[220px] sm:max-w-[280px] px-2 sm:px-8">
+                        <div className="bg-[#266C8B] min-h-10 text-white -translate-y-[50%] tracking-tighter py-2 px-2 box-content">
+                            <h3 className="translate-y-[50%]">Definição de Variáveis-chave</h3>
+                        </div>
+                        <p className="sm:text-lg">Identificamos as métricas essenciais para sua investigação, garantindo que a análise final seja 100% relevante e estratégica.</p>
+                    </div>
+                    <div className="bg-white shadow shadow-neutral-900 aspect-10/11 max-w-[220px] sm:max-w-[280px] px-2 sm:px-8">
+                        <div className="bg-[#266C8B] min-h-10 text-white -translate-y-[50%] tracking-tighter py-2 px-2 box-content">
+                            <h3 className="translate-y-[50%]">Cálculo Amostral Estatístico</h3>
+                        </div>
+                        <p className="sm:text-lg">Definimos o público e o tamanho exato da amostra para que seus resultados sejam representativos e possam ser generalizados com segurança.</p>
+                    </div>
+                </div>
+            </section>
         </main>
     )
 }
